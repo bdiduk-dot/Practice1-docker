a426ca7e92dc02a6f2d3ef23eb20872f4367cc5e
Ponchik <btarasenko30@gmail.com>
2026-05-01 12:51:23 +0300

    add LoggingInterceptor, TransformInterceptor, HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { TrimPipe } from './common/pipes/trim.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Pipes
   app.useGlobalPipes(
     new TrimPipe(),
     new ValidationPipe({
@@ -13,6 +19,30 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Global Interceptors
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+  );
+
+  // Global Filters
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('MiniShop API')
+    .setDescription(
+      'REST API для навчального інтернет-магазину. ' +
+        'Автентифікація через JWT Bearer token.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
