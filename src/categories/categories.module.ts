7e14c723225ce4fc387ed7f5cc01f4491ff9fc1a
Ponchik <btarasenko30@gmail.com>
2026-05-01 12:34:11 +0300

    add JwtAuthGuard, RolesGuard, protect endpoints

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { Category } from './category.entity';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, JwtAuthGuard, RolesGuard],
   exports: [CategoriesService],
 })
 export class CategoriesModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { Category } from '../categories/category.entity';
 import { Product } from './product.entity';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category]), AuthModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, JwtAuthGuard, RolesGuard],
   exports: [ProductsService],
 })
 export class ProductsModule {}
